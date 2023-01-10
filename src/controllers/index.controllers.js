d063135033963f951afb383e5940d20edcccd390
Bruno <xcdrxcdr@gmail.com>
2023-01-09 23:27:03 -0500

    commit

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -3,6 +3,6 @@ import {pool} from '../db.js';
 export const index = (req,res)=>res.json({message : "welcome to my API"});
 
 export const ping = async (req,res)=>{
-    const  [result] = await pool.query('SELECT "Pong" AS result');
+    const  [result] = await pool.query('SELECT "Pong" as result');
     res.json(result[0]);
 };
