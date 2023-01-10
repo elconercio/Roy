e78c1401dfb1a8e082f918dd335cb8dac0c12b98
Bruno <xcdrxcdr@gmail.com>
2023-01-09 23:19:48 -0500

    commit

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,6 +1,6 @@
 import {pool} from '../db.js';
 
-export const index = (req,res)=>json({message : "welcome to my API"});
+export const index = (req,res)=>res.json({message : "welcome to my API"});
 
 export const ping = async (req,res)=>{
     const  [result] = await pool.query('SELECT "Pong" AS result');
