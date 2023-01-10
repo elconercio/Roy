95a311c6aa7529fb8a124db09283627a74ed2bca
Bruno <xcdrxcdr@gmail.com>
2023-01-09 23:37:54 -0500

    commit

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,6 +5,6 @@ import { index, ping } from '../controllers/index.controllers.js';
 const router = Router();
 
 router.get("/", index);
-router.get('/ping', ping);
+router.get("/ping", ping);
 
 export default router;
