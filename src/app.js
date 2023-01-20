192136f2a0f475b8d6f3f136181134dea1c56629
Bruno <xcdrxcdr@gmail.com>
2023-01-20 13:27:45 -0500

    agregndo cors

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ import express from "express"
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
+const cors = require('cors')
 const app = express();
 
+app.use(cors());//todo el mundo
 app.use(express.json()); //antes de que llegue a la ruta
 
 app.use("/",indexRoutes);
